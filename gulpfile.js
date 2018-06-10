8cec442ea7342dcb27c8ef0e2ce53bd05d2af2b9
Rafael Bardini <rafael@rbardini.com>
2018-06-10 16:32:40 +0200

    Add new landing page design

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const del = require('del')
 const gulp = require('gulp')
-const concat = require('gulp-concat')
 const minifyHtml = require('gulp-minify-html')
 const uglify = require('gulp-uglify')
 const uncss = require('gulp-uncss')
@@ -12,60 +11,31 @@ gulp.task('clean', () =>
 
 gulp.task('html', ['clean'], () =>
   gulp
-    .src('src/index.html')
+    .src('src/*.html')
     .pipe(minifyHtml())
     .pipe(gulp.dest('dist/'))
 )
 
 gulp.task('scripts', ['clean'], () =>
   gulp
-    .src([
-      'src/scripts/jquery.min.js',
-      'src/scripts/jquery.throttle-debounce.min.js',
-      'src/scripts/jquery.nav.min.js',
-      'src/scripts/owl-carousel.min.js',
-      'src/scripts/nivo-lightbox.min.js',
-      'src/scripts/bootstrap.min.js',
-      'src/scripts/wow.min.js',
-      'src/scripts/matchMedia.min.js',
-      'src/scripts/app.js'
-    ])
-    .pipe(concat('main.js'))
+    .src('src/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js/'))
+    .pipe(gulp.dest('dist/'))
 )
 
 gulp.task('styles', ['clean'], () =>
   gulp
-    .src([
-      'src/styles/elegant-icons.css',
-      'src/styles/bootstrap.min.css',
-      'src/styles/owl-carousel.css',
-      'src/styles/owl-theme.css',
-      'src/styles/nivo-lightbox.css',
-      'src/styles/nivo-theme.css',
-      'src/styles/animate.min.css',
-      'src/styles/styles.css',
-      'src/styles/responsive.css',
-      'src/styles/colors/amber-blue.css'
-    ])
-    .pipe(concat('main.css'))
+    .src('src/*.css')
     .pipe(uncss({
-      html: ['dist/index.html'],
-      ignore: [/\.nivo-lightbox-.*/, /\.owl-.*/]
+      html: ['dist/index.html']
     }))
     .pipe(csso())
-    .pipe(gulp.dest('dist/css/'))
+    .pipe(gulp.dest('dist/'))
 )
 
 gulp.task('assets', ['clean'], () =>
   gulp
-    .src([
-      'src/apple-touch-icon.png',
-      'src/favicon.ico',
-      'src/fonts/**/*',
-      'src/images/**/*'
-    ], { base: 'src/' })
+    .src('src/*.{ico,png,svg}')
     .pipe(gulp.dest('dist/'))
 )
 
